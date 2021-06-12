75d5a231014717754d2531bcaf3affd20bea2f01
Marcus Robinson <chef11rob@gmail.com>
2021-06-11 20:13:56 -0400

    minor bug

--- resources/recipes.py
+++ resources/recipes.py
@@ -63,7 +63,7 @@ def create_recipe():
     print("-------------")
 
      # you should see request body in your terminal :)
-    new_recipe = models.Recipe.create(name=current_user.id, recipe_name=pay['recipe_name'],steps=pay['steps'], likes=pay['likes'], comments=pay['comments'])
+    new_recipe = models.Recipe.create(name=pay['name'], recipe_name=pay['recipe_name'],steps=pay['steps'], likes=pay['likes'], comments=pay['comments'])
     print(new_recipe)
 
     recipe_dict = model_to_dict(new_recipe)
